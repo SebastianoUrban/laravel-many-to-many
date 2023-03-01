481633ffd0fa452eeba257f6d69fbd5132ff5079
SebastianoUrban <sebastiano.urban98@gmail.com>
2023-03-01 17:22:27 +0100

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             TypeSeeder::class,
-            ProjectsTableSeeder::class,
-            TecnologySeeder::class
+            TecnologySeeder::class,
+            ProjectsTableSeeder::class
         ]);
     }
 }
